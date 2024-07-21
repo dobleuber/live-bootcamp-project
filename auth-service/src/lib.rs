e88a8ef23eade8227e69ac50ad79033f1d88944f
Wbert Castro <wbert.castro@genius.com>
2024-07-21 18:05:33 -0500

    Task 6 implement routes

---
+++ auth-service/src/lib.rs
@@ -0,0 +1,53 @@
+use std::error::Error;
+
+use axum::{response::IntoResponse, routing::post, serve::Serve, Router};
+use reqwest::StatusCode;
+use tower_http::services::ServeDir;
+
+pub struct Application {
+    server: Serve<Router, Router>,
+    pub address: String,
+}
+
+impl Application {
+    pub async fn build(address: &str) -> Result<Self, Box<dyn Error>> {
+        let router = Router::new()
+            .nest_service("/", ServeDir::new("assets"))
+            .route("/signup", post(signup))
+            .route("/login", post(login))
+            .route("/verify-2fa", post(verify_2fa))
+            .route("/logout", post(logout))
+            .route("/verify-token", post(verify_token));
+
+        let listener = tokio::net::TcpListener::bind(address).await?;
+        let address = listener.local_addr()?.to_string();
+        let server = axum::serve(listener, router);
+
+        Ok(Self { server, address })
+    }
+
+    pub async fn run(self) -> Result<(), std::io::Error> {
+        println!("listening on {}", &self.address);
+        self.server.await
+    }
+}
+
+async fn signup() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn login() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn verify_2fa() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn logout() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
+
+async fn verify_token() -> impl IntoResponse {
+    StatusCode::OK.into_response()
+}
---
+++ auth-service/tests/api/helpers.rs
@@ -0,0 +1,76 @@
+use auth_service::Application;
+use reqwest::Client;
+
+pub struct TestApp {
+    pub address: String,
+    pub http_client: reqwest::Client,
+}
+
+impl TestApp {
+    pub async fn new() -> Self {
+        let app = Application::build("127.0.0.1:0")
+            .await
+            .expect("Failed to build the app");
+
+        let address = format!("http://{}", &app.address);
+
+        let _app = tokio::spawn(app.run());
+
+        let http_client = Client::new();
+
+        Self {
+            address,
+            http_client,
+        }
+    }
+
+    pub async fn get_root(&self) -> reqwest::Response {
+        self.http_client
+            .get(&format!("{}/", &self.address))
+            .send()
+            .await
+            .expect("Failed to execute request.")
+    }
+
+    pub async fn post_signup(&self) -> reqwest::Response {
+        self.http_client
+            .post(&format!("{}/signup", &self.address))
+            .send()
+            .await
+            .expect("Failed to execute request.")
+    }
+
+    pub async fn get_login(&self) -> reqwest::Response {
+        self.http_client
+            .post(&format!("{}/login", &self.address))
+            .send()
+            .await
+            .expect("Failed to execute request.")
+    }
+
+    pub async fn get_logout(&self) -> reqwest::Response {
+        self.http_client
+            .post(&format!("{}/logout", &self.address))
+            .send()
+            .await
+            .expect("Failed to execute request.")
+    }
+
+    pub async fn get_verify_2fa(&self) -> reqwest::Response {
+        self.http_client
+            .post(&format!("{}/verify-2fa", &self.address))
+            .send()
+            .await
+            .expect("Failed to execute request.")
+    }
+
+    pub async fn get_verify_token(&self) -> reqwest::Response {
+        self.http_client
+            .post(&format!("{}/verify-token", &self.address))
+            .send()
+            .await
+            .expect("Failed to execute request.")
+    }
+
+    // TODO: Implement helper functions for all other routes (signup, login, logout, verify-2fa, and verify-token)
+}
